2eb82404214ceccebfffc9dd69b728556d8df39c
MrRabbit <1993iweb@gmail.com>
2022-04-20 15:11:50 +0800

    feat: init

---
+++ craco.config.js
@@ -0,0 +1,35 @@
+const CracoLessPlugin = require('craco-less')
+const path = require('path')
+const resolve = (dir) => path.resolve(__dirname, dir)
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': resolve('src')
+    }
+  },
+  devServer: {
+    proxy: {}
+  },
+  babel: {
+    plugins: [
+      // ['import', { libraryName: 'antd', style: true }] //第一个 style 为 true ,需要配置 craco-less一起才能生效
+      // ['import', { libraryName: 'antd', libraryDirectory: 'es', style: "css" }],//第二种 style 为css ,不需要 craco-less
+    ]
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              // '@primary-color': '#1DA57A'
+            },
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ]
+}
