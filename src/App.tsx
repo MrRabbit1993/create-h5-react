f5056a141a64f2cdff10a6648e1e82dcfd5b16e1
MrRabbit <1993iweb@gmail.com>
2022-04-21 14:57:23 +0800

    feat: 配置lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Button } from 'antd-mobile'
-import "./index.less"
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom';
+import './index.less'
 function App() {
-  return (
-    <div className="App">
-      text
-      <Button color='primary'>Primary</Button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button color="primary">切换按钮</Button>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<App />} path="/">
+                        <Route element={<Home />} index />
+                        <Route element={<Teams />} path="teams">
+                        </Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>,
+        </div>
+    );
 }
 
 export default App;
